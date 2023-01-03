9222a333239641c033fe488793187d0741fb319e
Lyudmila Balakireva <luda171@users.noreply.github.com>
2023-01-03 14:03:49 -0700

    Update pulsefacility.sql

--- src/main/resources/pulsefacility.sql
+++ src/main/resources/pulsefacility.sql
@@ -42,7 +42,10 @@ status TEXT,
 projnode VARCHAR(15),
 expnode VARCHAR(15),
 wikinode VARCHAR(15),
-refresh_token VARCHAR(50)
+refresh_token VARCHAR(50),
+location VARCHAR(10) 
 )
 #Alter table osf_user_access_log add   refresh_token VARCHAR(50);
+#Alter table osf_user_access_log add   location VARCHAR(10);
+#.headers ON
 create view dedupexperiments as select experimentID,PI,location,facility,magnetsystem,experiment_title, proposal_title,proposal_number,max(dtstart) as dtstart,dtend,dtstamp,summary,support,dtupdate,pid,calID,localupdate from experiments group by pid;
